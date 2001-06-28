4a9934a5b42fc92cdc8cbd0e382d55e3f7740167
Daniel Veillard <veillard@src.gnome.org>
2001-06-28 14:24:41 +0000

    Oops forgot to add libxslt.h, Daniel

---
+++ libxslt/libxslt.h
@@ -0,0 +1,19 @@
+/*
+ * libxslt.h: internal header only used during the compilation of libxslt
+ *
+ * See COPYRIGHT for the status of this software
+ *
+ * Author: daniel@veillard.com
+ */
+
+#ifndef __XSLT_LIBXSLT_H__
+#define __XSLT_LIBXSLT_H__
+
+#if defined(WIN32) && !defined(__CYGWIN__)
+#include "xsltwin32config.h"
+#else
+#include "xsltconfig.h"
+#include <libxml/xmlversion.h>
+#endif
+
+#endif /* ! __XSLT_LIBXSLT_H__ */
