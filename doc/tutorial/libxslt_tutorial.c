c43b7da65788fb728cfb36d1f5909e0a4f5bcb11
MDT 2001 John Fleck <jfleck@inkstain.net>
2001-07-23 15:34:28 +0000

    updating libxslt tutorial to include param support
    Mon Jul 23 09:32:27 MDT 2001 John Fleck <jfleck@inkstain.net>
    
    	* updating libxslt tutorial to include param support

--- doc/tutorial/libxslt_tutorial.c
+++ doc/tutorial/libxslt_tutorial.c
@@ -21,6 +21,7 @@
  *
  */ 
 
+#include <string.h>
 #include <libxml/xmlmemory.h>
 #include <libxml/debugXML.h>
 #include <libxml/HTMLtree.h>
@@ -39,10 +40,15 @@ extern int xmlLoadExtDtdDefaultValue;
 
 static void usage(const char *name) {
     printf("Usage: %s [options] stylesheet file [file ...]\n", name);
+    printf("      --param name value : pass a (parameter,value) pair\n");
+
 }
 
 int
 main(int argc, char **argv) {
+	int i;
+	const char *params[16 + 1];
+	int nbparams = 0;
 	xsltStylesheetPtr cur = NULL;
 	xmlDocPtr doc, res;
 
@@ -50,12 +56,34 @@ main(int argc, char **argv) {
 		usage(argv[0]);
 		return(1);
 	}
+	
 
+ for (i = 1; i < argc; i++) {
+        if (argv[i][0] != '-')
+            break;
+	if ((!strcmp(argv[i], "-param")) ||
+                   (!strcmp(argv[i], "--param"))) {
+		i++;
+		params[nbparams++] = argv[i++];
+		params[nbparams++] = argv[i];
+		if (nbparams >= 16) {
+			fprintf(stderr, "too many params\n");
+			return (1);
+		}
+        }  else {
+            fprintf(stderr, "Unknown option %s\n", argv[i]);
+            usage(argv[0]);
+            return (1);
+        }
+    }
+
+	params[nbparams] = NULL;
 	xmlSubstituteEntitiesDefault(1);
 	xmlLoadExtDtdDefaultValue = 1;
-	cur = xsltParseStylesheetFile((const xmlChar *)argv[1]);
-	doc = xmlParseFile(argv[2]);
-	res = xsltApplyStylesheet(cur, doc, NULL);
+	cur = xsltParseStylesheetFile((const xmlChar *)argv[i]);
+	i++;
+	doc = xmlParseFile(argv[i]);
+	res = xsltApplyStylesheet(cur, doc, params);
 	xsltSaveResultToFile(stdout, res, cur);
 
 	xsltFreeStylesheet(cur);
