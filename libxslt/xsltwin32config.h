26849a92603d2af2e8b723e53943229d3e7eef8b
Daniel Veillard <veillard@src.gnome.org>
2001-09-12 20:51:09 +0000

    preparing for 1.0.4 updated and regenerated docs Daniel
    * configure.in: preparing for 1.0.4
    * doc/xslt.html doc/html/*: updated and regenerated docs
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.3"
+#define LIBXSLT_DOTTED_VERSION "1.0.4"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10003
+#define LIBXSLT_VERSION 10004
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10003"
+#define LIBXSLT_VERSION_STRING "10004"
 
 /**
  * WITH_XSLT_DEBUG:
