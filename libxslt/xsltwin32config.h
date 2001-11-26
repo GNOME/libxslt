28c19237e6f3a5148c760d8b783aa12ffdaa77ee
Daniel Veillard <veillard@src.gnome.org>
2001-11-26 12:15:02 +0000

    preparing release of 1.0.8 Daniel
    * configure.in libxslt/xsltwin32config.h: preparing release of 1.0.8
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.7"
+#define LIBXSLT_DOTTED_VERSION "1.0.8"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10007
+#define LIBXSLT_VERSION 10008
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10007"
+#define LIBXSLT_VERSION_STRING "10008"
 
 /**
  * WITH_XSLT_DEBUG:
