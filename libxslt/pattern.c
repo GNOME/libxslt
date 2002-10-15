117597ebabeb0c71e06f8d4e26344579b51ef243
Daniel Veillard <veillard@src.gnome.org>
2002-10-15 09:45:29 +0000

    fixed the behaviour of node() patter which didn't patch the one defined in
    * libxslt/pattern.c: fixed the behaviour of node() patter which
      didn't patch the one defined in XPath :-( . Closes bug #95793
    * tests/docs/Makefile.am tests/docs/bug-92.xml
      tests/general/Makefile.am tests/general/bug-92.*: added the
      example in the regression tests for this case
    Daniel

--- libxslt/pattern.c
+++ libxslt/pattern.c
@@ -939,17 +939,11 @@ wrong_index:
 		break;
             case XSLT_OP_NODE:
 		switch (node->type) {
-		    case XML_DOCUMENT_NODE:
-		    case XML_HTML_DOCUMENT_NODE:
-#ifdef LIBXML_DOCB_ENABLED
-		    case XML_DOCB_DOCUMENT_NODE:
-#endif
 		    case XML_ELEMENT_NODE:
 		    case XML_CDATA_SECTION_NODE:
 		    case XML_PI_NODE:
 		    case XML_COMMENT_NODE:
 		    case XML_TEXT_NODE:
-		    case XML_ATTRIBUTE_NODE:
 			break;
 		    default:
 			return(0);
