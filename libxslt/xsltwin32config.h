36ec239c2a443a8be5561b93705492100687c7fa
Daniel Veillard <veillard@src.gnome.org>
2002-11-17 17:34:43 +0000

    preparing 1.0.23 rebuilding the docs Daniel
    * configure.in: preparing 1.0.23
    * doc/*: rebuilding the docs
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.22"
+#define LIBXSLT_DOTTED_VERSION "1.0.23"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10022
+#define LIBXSLT_VERSION 10023
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10022"
+#define LIBXSLT_VERSION_STRING "10023"
 
 /**
  * WITH_XSLT_DEBUG:
