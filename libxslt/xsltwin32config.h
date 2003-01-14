aaf8d3ad675ca03a3c77fd2c0e513ded01c4aa92
Daniel Veillard <veillard@src.gnome.org>
2003-01-14 15:28:23 +0000

    preparing release 2.0.24 small update updated the news, rebuilt the APIs
    * configure.in: preparing release 2.0.24
    * libxslt.spec.in: small update
    * doc/*: updated the news, rebuilt the APIs descriptions
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.23"
+#define LIBXSLT_DOTTED_VERSION "1.0.24"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10023
+#define LIBXSLT_VERSION 10024
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10023"
+#define LIBXSLT_VERSION_STRING "10024"
 
 /**
  * WITH_XSLT_DEBUG:
