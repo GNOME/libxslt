0efbfa5cd2d8e67c6425d9a5b8fcc84a384a0ef1
Daniel Veillard <veillard@src.gnome.org>
2003-03-24 23:46:53 +0000

    Recompiled, Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.27"
+#define LIBXSLT_DOTTED_VERSION "1.0.28"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10027
+#define LIBXSLT_VERSION 10028
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10027"
+#define LIBXSLT_VERSION_STRING "10028"
 
 /**
  * WITH_XSLT_DEBUG:
