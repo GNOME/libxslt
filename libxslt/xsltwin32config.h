3ef907d093c686976702767570d01c81c1f1bcbd
Daniel Veillard <veillard@src.gnome.org>
2003-05-04 15:44:04 +0000

    preparing release 1.0.30 backported a patch from libxml2 updated and
    * NEWS configure.in : preparing release 1.0.30
    * doc/apibuild.py: backported a patch from libxml2
    * doc/*: updated and rebuilt the docs
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -21,21 +21,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.0.29"
+#define LIBXSLT_DOTTED_VERSION "1.0.30"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10029
+#define LIBXSLT_VERSION 10030
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10029"
+#define LIBXSLT_VERSION_STRING "10030"
 
 /**
  * WITH_XSLT_DEBUG:
