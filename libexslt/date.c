91249324be8a7f3de6989b70da887f659c47b0f0
William M. Brack <wbrack@src.gnome.org>
2003-07-14 04:59:57 +0000

    fixed bug 113520

--- libexslt/date.c
+++ libexslt/date.c
@@ -1291,15 +1291,15 @@ exsltDateCastDateToNumber (const exsltDateValPtr dt)
         return 0.0;
 
     if ((dt->type & XS_GYEAR) == XS_GYEAR) {
-        ret = (double)(_exsltDateCastYMToDays(dt) * SECS_PER_DAY);
+        ret = (double)_exsltDateCastYMToDays(dt) * SECS_PER_DAY;
     }
 
     /* add in days */
     if (dt->type == XS_DURATION) {
-        ret += (double)(dt->value.dur.day * SECS_PER_DAY);
+        ret += (double)dt->value.dur.day * SECS_PER_DAY;
         ret += dt->value.dur.sec;
     } else {
-        ret += (double)(dt->value.date.day * SECS_PER_DAY);
+        ret += (double)dt->value.date.day * SECS_PER_DAY;
         /* add in time */
         ret += TIME_TO_NUMBER(dt);
     }
