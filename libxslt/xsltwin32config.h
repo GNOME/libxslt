1c14dc53028151c9c10c3cc46edda189fe549aca
Daniel Veillard <veillard@src.gnome.org>
2003-12-10 16:21:26 +0000

    updated the docs, made release 1.1.1 Daniel
    * configure.in NEWS doc/*: updated the docs, made release 1.1.1
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -23,21 +23,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.0"
+#define LIBXSLT_DOTTED_VERSION "1.1.1"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10100
+#define LIBXSLT_VERSION 10101
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10100"
+#define LIBXSLT_VERSION_STRING "10101"
 
 /**
  * WITH_XSLT_DEBUG:
