58f11833f525bb6ef9911baa5c2a1da0ac0ea84d
Daniel Veillard <veillard@src.gnome.org>
2003-12-24 14:17:26 +0000

    prepared release of libxslt-1.1.2 some tweaking following the new EXSLT
    * configure.in doc/*: prepared release of libxslt-1.1.2
    * libxslt.spec.in doc/Makefile.am: some tweaking following the
      new EXSLT docs.
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -23,21 +23,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.1"
+#define LIBXSLT_DOTTED_VERSION "1.1.2"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10101
+#define LIBXSLT_VERSION 10102
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10101"
+#define LIBXSLT_VERSION_STRING "10102"
 
 /**
  * WITH_XSLT_DEBUG:
