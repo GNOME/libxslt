ddbbcaf473ad105286c8ff35a5626c435483e7da
William M. Brack <wbrack@src.gnome.org>
2004-08-28 04:00:26 +0000

    fixed small problem with key initialisation disturbing the transformation
    * libxslt/keys.c: fixed small problem with key initialisation
      disturbing the transformation context (bug 151201).
    * tests/general/bug-153.*, tests/general/Makefile.am,
      tests/docs/bug-153*, tests/docs/Makefile.am: added test case
      for above

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -44,7 +44,7 @@ extern "C" {
  *
  * extra version information, used to show a CVS compilation
  */
-#define LIBXML_VERSION_EXTRA "-CVS936"
+#define LIBXML_VERSION_EXTRA "-CVS938"
 
 /**
  * WITH_XSLT_DEBUG:
