980a02c4f73d777b283a3277bd7b2b720ca71f90
Daniel Veillard <veillard@src.gnome.org>
2004-11-01 14:58:20 +0000

    added a test case provided by Markus Bertheau breaking on libxml2-2.6.15
    * tests/general/bug-155.*, tests/general/Makefile.am,
      tests/docs/bug-155*, tests/docs/Makefile.am: added a test case
      provided by Markus Bertheau breaking on libxml2-2.6.15
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -44,7 +44,7 @@ extern "C" {
  *
  * extra version information, used to show a CVS compilation
  */
-#define LIBXML_VERSION_EXTRA "-CVS963"
+#define LIBXML_VERSION_EXTRA "-CVS964"
 
 /**
  * WITH_XSLT_DEBUG:
