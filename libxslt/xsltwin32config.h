e8ed43e8f6bdc17b634e0feca045481c6737bf29
Daniel Veillard <veillard@src.gnome.org>
2005-04-02 11:43:03 +0000

    preparing release of 1.1.14, updated docs and rebuilt. Daniel
    * configure.in NEWS doc/*: preparing release of 1.1.14, updated docs
      and rebuilt.
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -23,28 +23,28 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.13"
+#define LIBXSLT_DOTTED_VERSION "1.1.14"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10113
+#define LIBXSLT_VERSION 10114
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10113"
+#define LIBXSLT_VERSION_STRING "10114"
 
 /**
  * LIBXSLT_VERSION_EXTRA:
  *
  * extra version information, used to show a CVS compilation
  */
-#define LIBXSLT_VERSION_EXTRA "-CVS1008"
+#define LIBXSLT_VERSION_EXTRA "-CVS1010"
 
 /**
  * WITH_XSLT_DEBUG:
