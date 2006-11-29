04a6430382c02eb9842c39f90e8fc8ddc5f5e11c
Daniel Veillard <veillard@src.gnome.org>
2006-11-29 15:09:33 +0000

    preparing release of libxslt-1.1.19 Daniel
    * NEWS configure.in doc//*: preparing release of libxslt-1.1.19
    Daniel

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -23,21 +23,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.18"
+#define LIBXSLT_DOTTED_VERSION "1.1.19"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10118
+#define LIBXSLT_VERSION 10119
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10118"
+#define LIBXSLT_VERSION_STRING "10119"
 
 /**
  * LIBXSLT_VERSION_EXTRA:
