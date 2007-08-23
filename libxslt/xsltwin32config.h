2896318613d0fd5b0ea52d5f4478d7dae932984e
Daniel Veillard <veillard@src.gnome.org>
2007-08-23 15:32:58 +0000

    preparing release of 1.1.22 Daniel
    * configure.in doc/*: preparing release of 1.1.22
    Daniel
    
    svn path=/trunk/; revision=1442

--- libxslt/xsltwin32config.h
+++ libxslt/xsltwin32config.h
@@ -23,21 +23,21 @@ extern "C" {
  *
  * the version string like "1.2.3"
  */
-#define LIBXSLT_DOTTED_VERSION "1.1.21"
+#define LIBXSLT_DOTTED_VERSION "1.1.22"
 
 /**
  * LIBXSLT_VERSION:
  *
  * the version number: 1.2.3 value is 1002003
  */
-#define LIBXSLT_VERSION 10121
+#define LIBXSLT_VERSION 10122
 
 /**
  * LIBXSLT_VERSION_STRING:
  *
  * the version number string, 1.2.3 value is "1002003"
  */
-#define LIBXSLT_VERSION_STRING "10121"
+#define LIBXSLT_VERSION_STRING "10122"
 
 /**
  * LIBXSLT_VERSION_EXTRA:
