0a8c1d7190357eae966c7a4ab3fc3a41fb5a764d
Daniel Veillard <veillard@redhat.com>
2012-08-08 14:10:08 +0800

    Adding --system flag support to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -62,9 +62,26 @@ if test x$OBJ_DIR != x; then
     cd "$OBJ_DIR"
 fi
 
+EXTRA_ARGS=
+if test "x$1" = "x--system"; then
+    shift
+    prefix=/usr
+    libdir=$prefix/lib
+    sysconfdir=/etc
+    localstatedir=/var
+    if [ -d /usr/lib64 ]; then
+      libdir=$prefix/lib64
+    fi
+    EXTRA_ARGS="--prefix=$prefix --sysconfdir=$sysconfdir --localstatedir=$localstatedir --libdir=$libdir"
+    echo "Running ./configure with $EXTRA_ARGS $@"
+else
+    echo "I am going to run ./configure with no arguments - if you wish "
+    echo "to pass any to it, please specify them on the $0 command line."
+fi
+
 if test -z "$NOCONFIGURE"; then
-    echo Running configure "$@" ...
-    $srcdir/configure "$@"
+    echo Running configure $EXTRA_ARGS "$@" ...
+    $srcdir/configure $EXTRA_ARGS "$@"
     echo 
     echo "Now type 'make' to compile libxslt."
 fi
