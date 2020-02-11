GNOME/libxslt

commit 44496bf3ecc32757c78b5d8f12df571597f0e0c0

Exportability taint of the headers

libxslt/preproc.h
@@ -10,6 +10,7 @@
 #define __XML_XSLT_PRECOMP_H__
 
 #include <libxml/tree.h>
+#include "xsltexports.h"
 #include "xsltInternals.h"
 
 #ifdef __cplusplus
@@ -21,13 +22,13 @@ extern "C" {
  */
 extern const xmlChar *xsltExtMarker;
 
-xsltElemPreCompPtr xsltDocumentComp	(xsltStylesheetPtr style,
+XSLTPUBFUN xsltElemPreCompPtr XSLTCALL xsltDocumentComp	(xsltStylesheetPtr style,
 					 xmlNodePtr inst,
 					 xsltTransformFunction function);
 
-void		xsltStylePreCompute	(xsltStylesheetPtr style,
+XSLTPUBFUN void XSLTCALL		xsltStylePreCompute	(xsltStylesheetPtr style,
 					 xmlNodePtr inst);
-void		xsltFreeStylePreComps	(xsltStylesheetPtr style);
+XSLTPUBFUN void XSLTCALL		xsltFreeStylePreComps	(xsltStylesheetPtr style);
 #ifdef __cplusplus
 }
 #endif

libexslt/exslt.h
@@ -3,16 +3,17 @@
 #define __EXSLT_H__
 
 #include <libxml/tree.h>
+#include "exsltexports.h"
 #include "exsltconfig.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-LIBEXSLT_PUBLIC extern const char *exsltLibraryVersion;
-LIBEXSLT_PUBLIC extern const int exsltLibexsltVersion;
-LIBEXSLT_PUBLIC extern const int exsltLibxsltVersion;
-LIBEXSLT_PUBLIC extern const int exsltLibxmlVersion;
+EXSLTPUBVAR const char *exsltLibraryVersion;
+EXSLTPUBVAR const int exsltLibexsltVersion;
+EXSLTPUBVAR const int exsltLibxsltVersion;
+EXSLTPUBVAR const int exsltLibxmlVersion;
 
 /**
  * EXSLT_COMMON_NAMESPACE:
@@ -64,16 +65,16 @@ LIBEXSLT_PUBLIC extern const int exsltLibxmlVersion;
  */
 #define SAXON_NAMESPACE ((const xmlChar *) "http://icl.com/saxon")
 
-void LIBEXSLT_PUBLIC exsltCommonRegister (void);
-void LIBEXSLT_PUBLIC exsltMathRegister (void);
-void LIBEXSLT_PUBLIC exsltSetsRegister (void);
-void LIBEXSLT_PUBLIC exsltFuncRegister (void);
-void LIBEXSLT_PUBLIC exsltStrRegister (void);
-void LIBEXSLT_PUBLIC exsltDateRegister (void);
-void LIBEXSLT_PUBLIC exsltSaxonRegister (void);
-void LIBEXSLT_PUBLIC exsltDynRegister(void);
+EXSLTPUBFUN void EXSLTCALL exsltCommonRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltMathRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltSetsRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltFuncRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltStrRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltDateRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltSaxonRegister (void);
+EXSLTPUBFUN void EXSLTCALL exsltDynRegister(void);
 
-void LIBEXSLT_PUBLIC exsltRegisterAll (void);
+EXSLTPUBFUN void EXSLTCALL exsltRegisterAll (void);
 
 #ifdef __cplusplus
 }

libexslt/exsltconfig.h.in
@@ -50,23 +50,6 @@ extern "C" {
 #define ATTRIBUTE_UNUSED
 #endif
 
-/**
- * LIBEXSLT_PUBLIC:
- *
- * This macro is needed on Win32 when using MSVC. It enables the client code
- * to access exported variables. It should expand to nothing when compiling
- * this library itself, but must expand to __declspec(dllimport) when a
- * client includes the library header and that only if it links dynamically
- * against this library.
- */
-#if !defined LIBEXSLT_PUBLIC
-#if (defined (__CYGWIN__) || defined _MSC_VER) && !defined IN_LIBEXSLT && !defined LIBEXSLT_STATIC
-#define LIBEXSLT_PUBLIC __declspec(dllimport)
-#else
-#define LIBEXSLT_PUBLIC 
-#endif
-#endif
-
 #ifdef __cplusplus
 }
 #endif

libexslt/exsltexports.h
@@ -1,0 +1,92 @@
+/*
+ * exsltexports.h : macros for marking symbols as exportable/importable.
+ *
+ * See Copyright for the status of this software.
+ *
+ * igor@zlatkovic.com
+ */
+
+#ifndef __EXSLT_EXPORTS_H__
+#define __EXSLT_EXPORTS_H__
+
+/**
+ * EXSLTPUBFUN, EXSLTPUBVAR, EXSLTCALL
+ *
+ * Macros which declare an exportable function, an exportable variable and
+ * the calling convention used for functions.
+ *
+ * Please use an extra block for every platform/compiler combination when
+ * modifying this, rather than overlong #ifdef lines. This helps
+ * readability as well as the fact that different compilers on the same
+ * platform might need different definitions.
+ */
+
+#define EXSLTPUBFUN
+#define EXSLTPUBVAR
+#define EXSLTCALL
+
+/* Windows platform with MS compiler */
+#if defined(_WIN32) && defined(_MSC_VER)
+  #undef EXSLTPUBFUN
+  #undef EXSLTPUBVAR
+  #undef EXSLTCALL
+  #if defined(IN_LIBEXSLT) && !defined(LIBEXSLT_STATIC)
+    #define EXSLTPUBFUN __declspec(dllexport)
+    #define EXSLTPUBVAR __declspec(dllexport)
+  #else
+    #define EXSLTPUBFUN
+    #if !defined(LIBEXSLT_STATIC)
+      #define EXSLTPUBVAR __declspec(dllimport) extern
+    #else
+      #define EXSLTPUBVAR
+    #endif
+  #endif
+  #define EXSLTCALL __cdecl
+#endif
+
+/* Windows platform with Borland compiler */
+#if defined(_WIN32) && defined(__BORLANDC__)
+  #undef EXSLTPUBFUN
+  #undef EXSLTPUBVAR
+  #undef EXSLTCALL
+  #if defined(IN_LIBEXSLT) && !defined(LIBEXSLT_STATIC)
+    #define EXSLTPUBFUN __declspec(dllexport)
+    #define EXSLTPUBVAR __declspec(dllexport)
+  #else
+    #define EXSLTPUBFUN
+    #if !defined(LIBEXSLT_STATIC)
+      #define EXSLTPUBVAR __declspec(dllimport) extern
+    #else
+      #define EXSLTPUBVAR
+    #endif
+  #endif
+  #define EXSLTCALL __cdecl
+#endif
+
+/* Cygwin platform, GNU compiler */
+#if defined(_WIN32) && defined(__CYGWIN__)
+  #undef EXSLTPUBFUN
+  #undef EXSLTPUBVAR
+  #undef EXSLTCALL
+  #if defined(IN_LIBEXSLT) && !defined(LIBEXSLT_STATIC)
+    #define EXSLTPUBFUN __declspec(dllexport)
+    #define EXSLTPUBVAR __declspec(dllexport)
+  #else
+    #define EXSLTPUBFUN
+    #if !defined(LIBEXSLT_STATIC)
+      #define EXSLTPUBVAR __declspec(dllimport) extern
+    #else
+      #define EXSLTPUBVAR
+    #endif
+  #endif
+  #define EXSLTCALL __cdecl
+#endif
+
+/* Compatibility */
+#if !defined(LIBEXSLT_PUBLIC)
+#define LIBEXSLT_PUBLIC EXSLTPUBVAR
+#endif
+
+#endif /* __EXSLT_EXPORTS_H__ */
+
+

libxslt/attributes.h
@@ -10,19 +10,24 @@
 #define __XML_XSLT_ATTRIBUTES_H__
 
 #include <libxml/tree.h>
+#include "xsltexports.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void	xsltParseStylesheetAttributeSet	(xsltStylesheetPtr style,
+XSLTPUBFUN void XSLTCALL
+	xsltParseStylesheetAttributeSet	(xsltStylesheetPtr style,
 					 xmlNodePtr cur);
-void	xsltFreeAttributeSetsHashes	(xsltStylesheetPtr style);
-void	xsltApplyAttributeSet		(xsltTransformContextPtr ctxt,
+XSLTPUBFUN void XSLTCALL    
+	xsltFreeAttributeSetsHashes	(xsltStylesheetPtr style);
+XSLTPUBFUN void XSLTCALL	
+	xsltApplyAttributeSet		(xsltTransformContextPtr ctxt,
 					 xmlNodePtr node,
 					 xmlNodePtr inst,
 					 xmlChar *attributes);
-void	xsltResolveStylesheetAttributeSet(xsltStylesheetPtr style);
+XSLTPUBFUN void XSLTCALL	
+	xsltResolveStylesheetAttributeSet(xsltStylesheetPtr style);
 #ifdef __cplusplus
 }
 #endif

libxslt/documents.h
@@ -10,25 +10,27 @@
 #define __XML_XSLT_DOCUMENTS_H__
 
 #include <libxml/tree.h>
+#include "xsltexports.h"
 #include "xsltInternals.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-xsltDocumentPtr	xsltNewDocument		(xsltTransformContextPtr ctxt,
+XSLTPUBFUN xsltDocumentPtr XSLTCALL	
+		xsltNewDocument		(xsltTransformContextPtr ctxt,
 					 xmlDocPtr doc);
-xsltDocumentPtr	xsltLoadDocument	(xsltTransformContextPtr ctxt,
+XSLTPUBFUN xsltDocumentPtr XSLTCALL	xsltLoadDocument	(xsltTransformContextPtr ctxt,
 					 const xmlChar *URI);
-xsltDocumentPtr	xsltFindDocument	(xsltTransformContextPtr ctxt,
+XSLTPUBFUN xsltDocumentPtr XSLTCALL	xsltFindDocument	(xsltTransformContextPtr ctxt,
 					 xmlDocPtr doc);
-void		xsltFreeDocuments	(xsltTransformContextPtr ctxt);
+XSLTPUBFUN void XSLTCALL		xsltFreeDocuments	(xsltTransformContextPtr ctxt);
 
-xsltDocumentPtr	xsltLoadStyleDocument	(xsltStylesheetPtr style,
+XSLTPUBFUN xsltDocumentPtr XSLTCALL	xsltLoadStyleDocument	(xsltStylesheetPtr style,
 					 const xmlChar *URI);
-xsltDocumentPtr	xsltNewStyleDocument	(xsltStylesheetPtr style,
+XSLTPUBFUN xsltDocumentPtr XSLTCALL	xsltNewStyleDocument	(xsltStylesheetPtr style,
 					 xmlDocPtr doc);
-void		xsltFreeStyleDocuments	(xsltStylesheetPtr style);
+XSLTPUBFUN void XSLTCALL		xsltFreeStyleDocuments	(xsltStylesheetPtr style);
 
 #ifdef __cplusplus
 }

libxslt/extensions.h
@@ -10,6 +10,7 @@
 #define __XML_XSLT_EXTENSION_H__
 
 #include "libxml/xpath.h"
+#include "xsltexports.h"
 #include "xsltInternals.h"
 
 #ifdef __cplusplus
@@ -68,45 +69,46 @@ typedef void (*xsltExtShutdownFunction) (xsltTransformContextPtr ctxt,
 					 const xmlChar *URI,
 					 void *data);
 
-int		xsltRegisterExtModule	(const xmlChar *URI,
+XSLTPUBFUN int XSLTCALL
+		xsltRegisterExtModule	(const xmlChar *URI,
 					 xsltExtInitFunction initFunc,
 					 xsltExtShutdownFunction shutdownFunc);
-int		xsltRegisterExtModuleFull
+XSLTPUBFUN int XSLTCALL		xsltRegisterExtModuleFull
 				(const xmlChar * URI,
 				 xsltExtInitFunction initFunc,
 				 xsltExtShutdownFunction shutdownFunc,
 				 xsltStyleExtInitFunction styleInitFunc,
 				 xsltStyleExtShutdownFunction styleShutdownFunc);
 
-int		xsltUnregisterExtModule	(const xmlChar * URI);
+XSLTPUBFUN int XSLTCALL		xsltUnregisterExtModule	(const xmlChar * URI);
 
-void *		xsltGetExtData		(xsltTransformContextPtr ctxt,
+XSLTPUBFUN void * XSLTCALL		xsltGetExtData		(xsltTransformContextPtr ctxt,
 					 const xmlChar *URI);
 
-void *		xsltStyleGetExtData	(xsltStylesheetPtr style,
+XSLTPUBFUN void * XSLTCALL		xsltStyleGetExtData	(xsltStylesheetPtr style,
 					 const xmlChar *URI);
 
-void		xsltShutdownCtxtExts	(xsltTransformContextPtr ctxt);
+XSLTPUBFUN void XSLTCALL		xsltShutdownCtxtExts	(xsltTransformContextPtr ctxt);
 
-void		xsltShutdownExts	(xsltStylesheetPtr style);
+XSLTPUBFUN void XSLTCALL		xsltShutdownExts	(xsltStylesheetPtr style);
 
-xsltTransformContextPtr xsltXPathGetTransformContext
+XSLTPUBFUN xsltTransformContextPtr XSLTCALL xsltXPathGetTransformContext
 					(xmlXPathParserContextPtr ctxt);
 
 /*
  * extension functions
 */
-int	xsltRegisterExtModuleFunction	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltRegisterExtModuleFunction	(const xmlChar *name,
 					 const xmlChar *URI,
 					 xmlXPathFunction function);
-xmlXPathFunction
+XSLTPUBFUN xmlXPathFunction XSLTCALL
 	xsltExtFunctionLookup		(xsltTransformContextPtr ctxt,
 					 const xmlChar *name,
 					 const xmlChar *URI);
-xmlXPathFunction
+XSLTPUBFUN xmlXPathFunction XSLTCALL
 	xsltExtModuleFunctionLookup	(const xmlChar *name,
 					 const xmlChar *URI);
-int	xsltUnregisterExtModuleFunction	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltUnregisterExtModuleFunction	(const xmlChar *name,
 					 const xmlChar *URI);
 
 /*
@@ -117,27 +119,27 @@ typedef xsltElemPreCompPtr (*xsltPreComputeFunction)
 					 xmlNodePtr inst,
 					 xsltTransformFunction function);
 
-xsltElemPreCompPtr xsltNewElemPreComp	(xsltStylesheetPtr style,
+XSLTPUBFUN xsltElemPreCompPtr XSLTCALL xsltNewElemPreComp	(xsltStylesheetPtr style,
 					 xmlNodePtr inst,
 					 xsltTransformFunction function);
-void	xsltInitElemPreComp		(xsltElemPreCompPtr comp,
+XSLTPUBFUN void XSLTCALL	xsltInitElemPreComp		(xsltElemPreCompPtr comp,
 					 xsltStylesheetPtr style,
 					 xmlNodePtr inst,
 					 xsltTransformFunction function,
 					 xsltElemPreCompDeallocator freeFunc);
 
-int	xsltRegisterExtModuleElement	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltRegisterExtModuleElement	(const xmlChar *name,
 					 const xmlChar *URI,
 					 xsltPreComputeFunction precomp,
 					 xsltTransformFunction transform);
-xsltTransformFunction xsltExtElementLookup(xsltTransformContextPtr ctxt,
+XSLTPUBFUN xsltTransformFunction XSLTCALL xsltExtElementLookup(xsltTransformContextPtr ctxt,
 					 const xmlChar *name,
 					 const xmlChar *URI);
-xsltTransformFunction xsltExtModuleElementLookup(const xmlChar *name,
+XSLTPUBFUN xsltTransformFunction XSLTCALL xsltExtModuleElementLookup(const xmlChar *name,
 					 const xmlChar *URI);
-xsltPreComputeFunction xsltExtModuleElementPreComputeLookup (const xmlChar *name,
+XSLTPUBFUN xsltPreComputeFunction XSLTCALL xsltExtModuleElementPreComputeLookup (const xmlChar *name,
 					 const xmlChar *URI);
-int	xsltUnregisterExtModuleElement	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltUnregisterExtModuleElement	(const xmlChar *name,
 					 const xmlChar *URI);
 
 /*
@@ -146,21 +148,21 @@ int	xsltUnregisterExtModuleElement	(const xmlChar *name,
 typedef void (*xsltTopLevelFunction)	(xsltStylesheetPtr style,
 					 xmlNodePtr inst);
 
-int	xsltRegisterExtModuleTopLevel	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltRegisterExtModuleTopLevel	(const xmlChar *name,
 					 const xmlChar *URI,
 					 xsltTopLevelFunction function);
-xsltTopLevelFunction xsltExtModuleTopLevelLookup (const xmlChar *name,
+XSLTPUBFUN xsltTopLevelFunction XSLTCALL xsltExtModuleTopLevelLookup (const xmlChar *name,
 					 const xmlChar *URI);
-int	xsltUnregisterExtModuleTopLevel	(const xmlChar *name,
+XSLTPUBFUN int XSLTCALL	xsltUnregisterExtModuleTopLevel	(const xmlChar *name,
 					 const xmlChar *URI);
 
 
 /* These 2 functions are deprecated for use within modules. */
-int		xsltRegisterExtFunction	(xsltTransformContextPtr ctxt,
+XSLTPUBFUN int XSLTCALL		xsltRegisterExtFunction	(xsltTransformContextPtr ctxt,
 					 const xmlChar *name,
 					 const xmlChar *URI,
 					 xmlXPathFunction function);
-int		xsltRegisterExtElement	(xsltTransformContextPtr ctxt,
+XSLTPUBFUN int XSLTCALL		xsltRegisterExtElement	(xsltTransformContextPtr ctxt,
 					 const xmlChar *name,
 					 const xmlChar *URI,
 					 xsltTransformFunction function);
@@ -170,29 +172,29 @@ int		xsltRegisterExtElement	(xsltTransformContextPtr ctxt,
  * Those are used by the XSLT (pre)processor.
  */
 
-int		xsltRegisterExtPrefix	(xsltStylesheetPtr style,
+XSLTPUBFUN int XSLTCALL		xsltRegisterExtPrefix	(xsltStylesheetPtr style,
 					 const xmlChar *prefix,
 					 const xmlChar *URI);
-int		xsltCheckExtPrefix	(xsltStylesheetPtr style,
+XSLTPUBFUN int XSLTCALL		xsltCheckExtPrefix	(xsltStylesheetPtr style,
 					 const xmlChar *prefix);
-int		xsltInitCtxtExts	(xsltTransformContextPtr ctxt);
-void		xsltFreeCtxtExts	(xsltTransformContextPtr ctxt);
-void		xsltFreeExts		(xsltStylesheetPtr style);
+XSLTPUBFUN int XSLTCALL		xsltInitCtxtExts	(xsltTransformContextPtr ctxt);
+XSLTPUBFUN void XSLTCALL		xsltFreeCtxtExts	(xsltTransformContextPtr ctxt);
+XSLTPUBFUN void XSLTCALL		xsltFreeExts		(xsltStylesheetPtr style);
 
-xsltElemPreCompPtr xsltPreComputeExtModuleElement(xsltStylesheetPtr style,
+XSLTPUBFUN xsltElemPreCompPtr XSLTCALL xsltPreComputeExtModuleElement(xsltStylesheetPtr style,
 					 xmlNodePtr inst);
 /*
  * Extension Infos access.
  * Used by exslt initialisation
  */
 
-xmlHashTablePtr xsltGetExtInfo (xsltStylesheetPtr style, const xmlChar *URI);
+XSLTPUBFUN xmlHashTablePtr XSLTCALL xsltGetExtInfo (xsltStylesheetPtr style, const xmlChar *URI);
 
 /**
  * Test module http://xmlsoft.org/XSLT/
  */
-void	xsltRegisterTestModule		(void);
-void	xsltDebugDumpExtensions(FILE * output);
+XSLTPUBFUN void XSLTCALL	xsltRegisterTestModule		(void);
+XSLTPUBFUN void XSLTCALL	xsltDebugDumpExtensions(FILE * output);
 
 
 #ifdef __cplusplus

libxslt/extra.h
@@ -10,6 +10,7 @@
 #define __XML_XSLT_EXTRA_H__
 
 #include "libxml/xpath.h"
+#include "xsltexports.h"
 #include "xsltInternals.h"
 
 #ifdef __cplusplus
@@ -54,16 +55,17 @@ extern "C" {
 	"http://nwalsh.com/xslt/ext/com.nwalsh.saxon.CVS")
 
 
-void		xsltFunctionNodeSet	(xmlXPathParserContextPtr ctxt,
+XSLTPUBFUN void XSLTCALL
+		xsltFunctionNodeSet	(xmlXPathParserContextPtr ctxt,
 					 int nargs);
-void		xsltDebug		(xsltTransformContextPtr ctxt,
+XSLTPUBFUN void XSLTCALL		xsltDebug		(xsltTransformContextPtr ctxt,
 					 xmlNodePtr node,
 					 xmlNodePtr inst,
 					 xsltStylePreCompPtr comp);
 
 
-void		xsltRegisterExtras	(xsltTransformContextPtr ctxt);
-void		xsltRegisterAllExtras	(void);
+XSLTPUBFUN void XSLTCALL		xsltRegisterExtras	(xsltTransformContextPtr ctxt);
+XSLTPUBFUN void XSLTCALL		xsltRegisterAllExtras	(void);
 
 #ifdef __cplusplus
 }

commit 0141e19eef59863ceac7fcb00345bc9432e3bf11

Fix export of xsltExtMarker on Windows
* libxslt/preproc.h:
(xsltExtMarker): Change `extern` to `XSLTPUBVAR` so this
variable is exported consistently on all platforms.  This
regressed in commit 44496bf3e.

libxslt/preproc.h
@@ -22,7 +22,7 @@ extern "C" {
 /*
  * Interfaces
  */
-extern const xmlChar *xsltExtMarker;
+XSLTPUBVAR const xmlChar *xsltExtMarker;
 
 XSLTPUBFUN xsltElemPreCompPtr XSLTCALL
 		xsltDocumentComp	(xsltStylesheetPtr style,