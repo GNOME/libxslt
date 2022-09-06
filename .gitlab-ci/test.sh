78e0bf427715d885d1a1d53df45a5d6535536a36
Nick Wellnhofer <wellnhofer@aevum.de>
2022-09-06 13:14:52 +0200

    gitlab-ci: Add CI job for MinGW/Autotools
    - Move some common scripts to .gitlab-ci
    - Simplify test.sh

---
+++ .gitlab-ci/test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+
+git clone --depth 1 https://gitlab.gnome.org/GNOME/libxml2.git
+cd libxml2
+sh autogen.sh $CONFIG
+make -j$(nproc)
+cd ..
+
+sh autogen.sh $CONFIG --with-libxml-src=libxml2
+make -j$(nproc) V=1 CFLAGS="$CFLAGS -Werror"
+make -s -C tests tests | tee test.log
+make -s -C xsltproc tests | tee -a test.log
+grep -qv '^## Running' test.log && exit 1
+make -s -C python tests
