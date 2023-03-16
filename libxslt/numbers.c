GNOME/libxslt

commit f13329a622f0370bf1eb72ef99229e8faf46f732

numbers: Fix another floating point overflow
Check for infinity after applying multiplier.

libxslt/numbers.c
@@ -985,27 +985,12 @@ xsltFormatNumberConversion(xsltDecimalFormatPtr self,
 		"Invalid format (0-length)\n");
     }
     *result = NULL;
-    switch (xmlXPathIsInf(number)) {
-	case -1:
-	    if (self->minusSign == NULL)
-		*result = xmlStrdup(BAD_CAST "-");
-	    else
-		*result = xmlStrdup(self->minusSign);
-	    /* Intentional fall-through */
-	case 1:
-	    if ((self == NULL) || (self->infinity == NULL))
-		*result = xmlStrcat(*result, BAD_CAST "Infinity");
-	    else
-		*result = xmlStrcat(*result, self->infinity);
-	    return(status);
-	default:
-	    if (xmlXPathIsNaN(number)) {
-		if ((self == NULL) || (self->noNumber == NULL))
-		    *result = xmlStrdup(BAD_CAST "NaN");
-		else
-		    *result = xmlStrdup(self->noNumber);
-		return(status);
-	    }
+    if (xmlXPathIsNaN(number)) {
+        if ((self == NULL) || (self->noNumber == NULL))
+            *result = xmlStrdup(BAD_CAST "NaN");
+        else
+            *result = xmlStrdup(self->noNumber);
+        return(status);
     }
 
     buffer = xmlBufferCreate();
@@ -1283,6 +1268,25 @@ OUTPUT_NUMBER:
 	format_info.add_decimal = TRUE;
     }
 
+    /* Apply multiplier */
+    number *= (double)format_info.multiplier;
+    switch (xmlXPathIsInf(number)) {
+	case -1:
+	    if (self->minusSign == NULL)
+		*result = xmlStrdup(BAD_CAST "-");
+	    else
+		*result = xmlStrdup(self->minusSign);
+	    /* Intentional fall-through */
+	case 1:
+	    if ((self == NULL) || (self->infinity == NULL))
+		*result = xmlStrcat(*result, BAD_CAST "Infinity");
+	    else
+		*result = xmlStrcat(*result, self->infinity);
+	    return(status);
+	default:
+            break;
+    }
+
     /* Ready to output our number.  First see if "default sign" is required */
     if (default_sign != 0)
 	xmlBufferAdd(buffer, self->minusSign, xmlUTF8Strsize(self->minusSign, 1));
@@ -1297,11 +1301,13 @@ OUTPUT_NUMBER:
         j += len;
     }
 
-    /* Next do the integer part of the number */
-    number = fabs(number) * (double)format_info.multiplier;
+    /* Round to n digits */
+    number = fabs(number);
     scale = pow(10.0, (double)(format_info.frac_digits + format_info.frac_hash));
     number += .5 / scale;
     number -= fmod(number, 1 / scale);
+
+    /* Next do the integer part of the number */
     if ((self->grouping != NULL) &&
         (self->grouping[0] != 0)) {
         int gchar;

commit 5d452a61d3580054c45501f761f233d34b8a29d3

numbers: Fix memory leak in xsltFormatNumberConversion
Short-lived regression from f13329a6.

Found by OSS-Fuzz.

libxslt/numbers.c
@@ -993,11 +993,6 @@ xsltFormatNumberConversion(xsltDecimalFormatPtr self,
         return(status);
     }
 
-    buffer = xmlBufferCreate();
-    if (buffer == NULL) {
-	return XPATH_MEMORY_ERROR;
-    }
-
     format_info.integer_hash = 0;
     format_info.integer_digits = 0;
     format_info.frac_digits = 0;
@@ -1285,6 +1280,11 @@ OUTPUT_NUMBER:
 	    return(status);
 	default:
             break;
+    }
+
+    buffer = xmlBufferCreate();
+    if (buffer == NULL) {
+	return XPATH_MEMORY_ERROR;
     }
 
     /* Ready to output our number.  First see if "default sign" is required */