GNOME/libxslt

commit 014b28927c25684908828494fb2ad0421f7c559a

extensions: Don't call deprecated xmlCheckFilename
If the file doesn't exist, xmlModuleOpen will fail.

libxslt/extensions.c
@@ -415,17 +415,6 @@ xsltExtModuleRegisterDynamic(const xmlChar * URI)
                      module_filename, URI);
 #endif
 
-    if (1 != xmlCheckFilename(module_filename)) {
-
-#ifdef WITH_XSLT_DEBUG_EXTENSIONS
-	xsltGenericDebug(xsltGenericDebugContext,
-                     "xmlCheckFilename failed for plugin: %s\n", module_filename);
-#endif
-
-        xmlFree(ext_name);
-        return (-1);
-    }
-
     /* attempt to open the module */
     m = xmlModuleOpen(module_filename, 0);
     if (NULL == m) {

commit a85bd17ec52793b33074f0f785c3d989feb20170

extensions: Readd call to xmlCheckFilename with older libxml2
Fix spurious warning with libxml2 before 2.13.0 caused by 014b2892.

Fixes #113.

libxslt/extensions.c
@@ -415,6 +415,19 @@ xsltExtModuleRegisterDynamic(const xmlChar * URI)
                      module_filename, URI);
 #endif
 
+#if LIBXML_VERSION < 21300
+    if (1 != xmlCheckFilename(module_filename)) {
+
+#ifdef WITH_XSLT_DEBUG_EXTENSIONS
+	xsltGenericDebug(xsltGenericDebugContext,
+                     "xmlCheckFilename failed for plugin: %s\n", module_filename);
+#endif
+
+        xmlFree(ext_name);
+        return (-1);
+    }
+#endif
+
     /* attempt to open the module */
     m = xmlModuleOpen(module_filename, 0);
     if (NULL == m) {