ae147fefb408852567d40f1b4abd77a8c0e06e71
Nick Wellnhofer <wellnhofer@aevum.de>
2024-12-09 23:02:56 +0100

    gitlab-ci: Add "dist" job to build distribution tarball
    Prepare for GNOME Release Service.

---
+++ .gitlab-ci/dist.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+LIBXML2_PREFIX="$CI_PROJECT_DIR/libxml2/install"
+git clone --depth 1 https://gitlab.gnome.org/GNOME/libxml2.git
+cd libxml2
+sh autogen.sh --prefix="$LIBXML2_PREFIX"
+make -j$(nproc)
+make install
+cd ..
+
+mkdir -p libxslt-dist
+cd libxslt-dist
+sh ../autogen.sh --with-libxml-prefix="$LIBXML2_PREFIX"
+export PYTHONPATH="$(echo $LIBXML2_PREFIX/lib/python*/site-packages):$PYTHONPATH"
+make distcheck V=1 DISTCHECK_CONFIGURE_FLAGS=" \
+    --with-crypto \
+    --with-plugins \
+    --with-libxml-prefix=$LIBXML2_PREFIX \
+"
+if [ -z "$CI_COMMIT_TAG" ]; then
+    mv libxslt-*.tar.xz libxslt-git-$CI_COMMIT_SHORT_SHA.tar.xz
+fi
