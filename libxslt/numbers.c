9aeca0fcf6f0297eee6663e11f2014d9031b5072
Nick Wellnhofer <wellnhofer@aevum.de>
2024-12-19 19:24:25 +0100

    numbers: Stop using libxml2 XPath axis API
    These internals were never meant for public consumption and will be
    deprecated in libxml2. Handling traversal directly is also a lot easier
    except for XPath namespace nodes.

--- libxslt/numbers.c
+++ libxslt/numbers.c
@@ -680,51 +680,48 @@ xsltNumberFormatGetMultipleLevel(xsltTransformContextPtr context,
 {
     int amount = 0;
     int cnt;
-    xmlNodePtr oldCtxtNode;
     xmlNodePtr ancestor;
     xmlNodePtr preceding;
-    xmlXPathParserContextPtr parser;
 
-    oldCtxtNode = context->xpathCtxt->node;
-    parser = xmlXPathNewParserContext(NULL, context->xpathCtxt);
-    if (parser) {
-	/* ancestor-or-self::*[count] */
-	ancestor = node;
-	while ((ancestor != NULL) && (ancestor->type != XML_DOCUMENT_NODE)) {
-	    if ((fromPat != NULL) &&
-		xsltTestCompMatchList(context, ancestor, fromPat))
-		break; /* for */
+    /* ancestor-or-self::*[count] */
+    ancestor = node;
 
-            /*
-             * The xmlXPathNext* iterators require that the context node is
-             * set to the start node. Calls to xsltTestCompMatch* may also
-             * leave the context node in an undefined state, so make sure
-             * that the context node is reset before each iterator invocation.
-             */
+    while ((ancestor != NULL) && (ancestor->type != XML_DOCUMENT_NODE)) {
+        if ((fromPat != NULL) &&
+            xsltTestCompMatchList(context, ancestor, fromPat))
+            break;
 
-	    if (xsltTestCompMatchCount(context, ancestor, countPat, node)) {
-		/* count(preceding-sibling::*) */
-		cnt = 1;
-                context->xpathCtxt->node = ancestor;
-                preceding = xmlXPathNextPrecedingSibling(parser, ancestor);
-                while (preceding != NULL) {
-	            if (xsltTestCompMatchCount(context, preceding, countPat,
-                                               node))
-			cnt++;
-                    context->xpathCtxt->node = ancestor;
-                    preceding =
-                        xmlXPathNextPrecedingSibling(parser, preceding);
-		}
-		array[amount++] = (double)cnt;
-		if (amount >= max)
-		    break; /* for */
-	    }
-            context->xpathCtxt->node = node;
-            ancestor = xmlXPathNextAncestor(parser, ancestor);
-	}
-	xmlXPathFreeParserContext(parser);
+        if (xsltTestCompMatchCount(context, ancestor, countPat, node)) {
+            /* count(preceding-sibling::*) */
+            cnt = 1;
+            if (ancestor->type != XML_NAMESPACE_DECL)
+                preceding = ancestor->prev;
+            else
+                preceding = NULL;
+            while (preceding != NULL) {
+                if (xsltTestCompMatchCount(context, preceding, countPat,
+                                           node))
+                    cnt++;
+                preceding = preceding->prev;
+            }
+            array[amount++] = (double)cnt;
+            if (amount >= max)
+                break;
+        }
+
+        if ((ancestor != NULL) && (ancestor->type == XML_NAMESPACE_DECL)) {
+            xmlNsPtr ns = (xmlNsPtr) ancestor;
+
+            if ((ns->next != NULL) &&
+                (ns->next->type != XML_NAMESPACE_DECL))
+                ancestor = (xmlNodePtr) ns->next;
+            else
+                ancestor = NULL;
+        } else {
+            ancestor = ancestor->parent;
+        }
     }
-    context->xpathCtxt->node = oldCtxtNode;
+
     return amount;
 }
 
