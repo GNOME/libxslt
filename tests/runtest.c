5389820088ae126efa598153d02a4f777ccac18d
Iván Chavero <ichavero@chavero.com.mx>
2025-10-03 08:47:34 -0600

    Fix windows warning

--- tests/runtest.c
+++ tests/runtest.c
@@ -16,6 +16,7 @@
   #define WIN32_LEAN_AND_MEAN
   #include <windows.h>
   #include <io.h>
+  #include <direct.h>
 #else
   #include <unistd.h>
   #include <glob.h>
@@ -442,7 +443,7 @@ testStructuredErrorHandler(void *ctx ATTRIBUTE_UNUSED, const xmlError *err) {
 static void
 initializeLibxml2(void) {
     xmlInitParser();
-    /*TODO: Update this code for the thread safe versions based on
+    /* TODO: Update this code for the thread safe versions based on
      * the xmlCtxtSetResourceLoader function
      */
     xmlSetExternalEntityLoader(xmlGetExternalEntityLoader());
